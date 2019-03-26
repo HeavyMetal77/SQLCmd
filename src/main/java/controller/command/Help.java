c76634fa0be5f8550d7e6c29e682b956dc8b2f34
HeavyMetal77 <ttm2006@ukr.net>
2019-03-26 15:54:27 +0200

    Команда help работает самостоятельным классом

---
+++ src/main/java/controller/command/Help.java
@@ -0,0 +1,39 @@
+package controller.command;
+
+import view.View;
+
+public class Help implements Command {
+    private View view;
+
+    public Help(View view) {
+        this.view = view;
+    }
+    @Override
+    public boolean canProcess(String command) {
+        return command.equals("help");
+    }
+
+    @Override
+    public void process(String command) {
+            view.write("Существующие команды:");
+
+            view.write("\ttables - ");
+            view.write("\t\tвывод списка всех таблиц");
+
+            view.write("\tclear - ");
+            view.write("\t\tочистка таблицы");
+
+            view.write("\tfind|tableName - ");
+            view.write("\t\tвывод содержимого таблицы tableName");
+
+            view.write("\tdrop|tableName - ");
+            view.write("\t\tудалить таблицу tableName");
+
+            view.write("\tcreateTable|tableName|column1|column2|...|columnN - ");
+            view.write("\t\tсоздать таблицу tableName с колонками column1...columnN ");
+
+            view.write("\texit - ");
+            view.write("\t\tвыход из программы");
+
+    }
+}
